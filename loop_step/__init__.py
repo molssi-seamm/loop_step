ac5b6cc19d4badf205f79e6dc8330f4b67532148
Paul Saxe <paul@thesaxes.net>
2019-09-04 19:46:22 -0400

    Moved to versioneer for versioning.

--- loop_step/__init__.py
+++ loop_step/__init__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for Loop Step."""
-
-__author__ = """Paul Saxe"""
-__email__ = 'psaxe@molssi.org'
-__version__ = '0.1.0'
+"""
+loop_step
+A step for loops in the SEAMM flowcharts
+"""
 
 # Bring up the classes so that they appear to be directly in
 # the loop_step package.
@@ -13,3 +12,12 @@ from loop_step.loop import Loop  # noqa: F401
 from loop_step.loop_parameters import LoopParameters  # noqa: F401
 from loop_step.loop_step import LoopStep  # noqa: F401
 from loop_step.tk_loop import TkLoop  # noqa: F401
+
+# Handle versioneer
+from ._version import get_versions
+__author__ = """Paul Saxe"""
+__email__ = 'psaxe@molssi.org'
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
