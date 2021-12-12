37f652fdce312207ebe524559efef10f9f774be9
Paul Saxe <paul@thesaxes.net>
2021-12-11 21:29:57 -0500

    Added query on column of tables, and handling of errors.

--- loop_step/tk_loop.py
+++ loop_step/tk_loop.py
@@ -62,7 +62,12 @@ class TkLoop(seamm.TkNode):
         for key in P:
             self[key] = P[key].widget(frame)
 
-        self["type"].combobox.bind("<<ComboboxSelected>>", self.reset_dialog)
+        self["type"].bind("<<ComboboxSelected>>", self.reset_dialog)
+        self["where"].bind("<<ComboboxSelected>>", self.reset_dialog)
+        self["query-op"].bind("<<ComboboxSelected>>", self.reset_dialog)
+
+        for widget in ("type", "where", "query-op", "errors"):
+            self[widget].combobox.config(state="readonly")
 
     def reset_dialog(self, widget=None):
         """Lay out the edit dialog according to the type of loop."""
@@ -80,20 +85,36 @@ class TkLoop(seamm.TkNode):
         # keep track of the row in a variable, so that the layout is flexible
         # if e.g. rows are skipped to control such as 'method' here
         row = 0
-        self["type"].grid(row=row, column=0, sticky=tk.W)
+        self["type"].grid(row=row, column=0, columnspan=2, sticky=tk.W)
         if loop_type == "For":
             self["variable"].grid(row=row, column=2, sticky=tk.W)
             self["start"].grid(row=row, column=3, sticky=tk.W)
             self["end"].grid(row=row, column=4, sticky=tk.W)
             self["step"].grid(row=row, column=5, sticky=tk.W)
         elif loop_type == "Foreach":
+            frame.columnconfigure(5, weight=0)
             self["variable"].grid(row=row, column=2, sticky=tk.W)
-            self["values"].grid(row=row, column=3, sticky=tk.W)
+            self["values"].grid(row=row, column=3, sticky=tk.EW)
+            frame.columnconfigure(3, weight=1)
         elif loop_type == "For rows in table":
-            self["table"].grid(row=row, column=2, sticky=tk.W)
+            frame.columnconfigure(3, weight=0)
+            self["table"].grid(row=row, column=2, columnspan=2, sticky=tk.EW)
+            row += 1
+            self["where"].grid(row=row, column=1, columnspan=2, sticky=tk.EW)
+            where = self["where"].get()
+            if where != "Use all rows":
+                self["query-column"].grid(row=row, column=3, sticky=tk.EW)
+                self["query-op"].grid(row=row, column=4)
+                op = self["query-op"].get()
+                if "empty" not in op:
+                    self["query-value"].grid(row=row, column=5, sticky=tk.EW)
+                    frame.columnconfigure(5, weight=1)
         else:
             raise RuntimeError("Don't recognize the loop_type {}".format(loop_type))
         row += 1
+        self["errors"].grid(row=row, column=0, columnspan=4, sticky=tk.W)
+        row += 1
+        frame.columnconfigure(0, minsize=40)
 
     def right_click(self, event):
         """Probably need to add our dialog..."""
